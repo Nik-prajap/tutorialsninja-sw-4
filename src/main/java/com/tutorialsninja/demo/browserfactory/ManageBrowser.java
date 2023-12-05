5468a74aa7ef93bea1ab547f8f3a49418c384d7c
nik-prajap <nik.prajap@gmail.com>
2023-12-05 23:47:50 +0000

    sw4tutorialninja

---
+++ src/main/java/com/tutorialsninja/demo/browserfactory/ManageBrowser.java
@@ -0,0 +1,38 @@
+package com.tutorialsninja.demo.browserfactory;
+
+import com.tutorialsninja.demo.propertyreader.PropertyReader;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class ManageBrowser {
+
+    public static WebDriver driver;
+    String baseUrl = PropertyReader.getInstance().getProperty("baseUrl");
+
+    public void selectBrowser(String browser) {
+
+        if (browser.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+        } else if (browser.equalsIgnoreCase("Firefox")) {
+            driver = new FirefoxDriver();
+        } else {
+            System.out.println("Wrong Browser Name");
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+        driver.get(baseUrl);
+    }
+
+    public void closeBrowser() {
+        if (driver!=null) {
+            driver.quit();
+        }
+    }
+}
